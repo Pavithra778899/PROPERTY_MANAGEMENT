c5a8130e0d82d54631f6bc0cf510c21f5c2bbe87
Pavithra778899 <vasupallipavithra@gmail.com>
2025-06-03 17:51:32 +0530

    Update prop_new.py

--- prop_new.py
+++ prop_new.py
@@ -640,7 +640,7 @@ else:
                 "Give me the total supplier amount year over year by month?",
                 "Give total supplier payment amount by payment purpose?",
                 "Give me the average supplier amount by property?",
-                "Give me the top 10 suppliers by payment amount by state and occupancy status?",
+                "Give me the  payment amount by state and occupancy status?",
                 "Budget recovery by billing purpose?",
                 "Give me the customer billing details?"
             ]
